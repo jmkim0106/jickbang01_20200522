9b536dedc9e2f2bd1dd68e3661736ad1e7e4ee50
weekyeon <weekyeon@gmail.com>
2020-05-22 15:20:36 +0900

    데이터 목록 추가 + 어댑터 생성 및 리스트 추가

--- app/src/main/java/com/example/jickbang01_20200522/MainActivity.java
+++ app/src/main/java/com/example/jickbang01_20200522/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.jickbang01_20200522.adapters.RoomAdepter;
 import com.example.jickbang01_20200522.databinding.ActivityMainBinding;
+import com.example.jickbang01_20200522.datas.Room;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    List<Room> rooms = new ArrayList<>();
+    RoomAdepter mRoomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,19 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        addRooms();
+
+        mRoomAdapter = new RoomAdepter(mContext, R.layout.room_list_item, rooms);
+        binding.roomListView.setAdapter(mRoomAdapter);
 
     }
+
+    void addRooms() {
+        rooms.add(new Room(7500, "은평구 불광동", 4, "불광동의 흔한 집입니다."));
+        rooms.add(new Room(28500, "마포구 서교동", 2, "신혼부부의 보금자리 서교동..."));
+        rooms.add(new Room(5500, "은평구 갈현동", 0, "갈현동 반지하 집입니다."));
+        rooms.add(new Room(3500, "은평구 응암동", -1, "응암동 지하 1층 집입니다."));
+        rooms.add(new Room(58500, "은평구 신사동", 5, "신사동 5층 집입니다."));
+    }
+
 }
